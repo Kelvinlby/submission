f79e3400a2304a11e641d275043d8fedbe69c80c
Kelvin <76610777+Kelvinlby@users.noreply.github.com>
2024-10-18 19:27:08 +0800

    update

--- lib/widgets/monitor/system_info.dart
+++ lib/widgets/monitor/system_info.dart
@@ -19,14 +19,34 @@ class _SystemInfoCardState extends State<SystemInfoCard> {
   Map<String, int> gpuInfo = {'GpuUsage': 0, 'TotalVram': 0, 'UsedVram': 0, 'VramUsage': 0};
   GpuMonitor monitor = GpuMonitor();
 
+  // Line chart config
+  double xValue = 0;
+  double step = 0.05;
+  final limitCount = 100;
+  final ramPoints = <FlSpot>[];
+  final vramPoints = <FlSpot>[];
+  final gpuPoints = <FlSpot>[];
+
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+    _timer = Timer.periodic(const Duration(milliseconds: 40), (timer) {
+      while (ramPoints.length > limitCount) {
+        ramPoints.removeAt(0);
+        vramPoints.removeAt(0);
+        gpuPoints.removeAt(0);
+      }
+
       setState(() {
         cpuInfo = monitor.getCpuInfo();
         gpuInfo = monitor.getGpuInfo();
+
+        ramPoints.add(FlSpot(xValue, cpuInfo['RamUsage']! / 100));
+        vramPoints.add(FlSpot(xValue, gpuInfo['VramUsage']! / 100));
+        gpuPoints.add(FlSpot(xValue, gpuInfo['GpuUsage']! / 100));
       });
+
+      xValue += step;
     });
   }
 
@@ -36,9 +56,45 @@ class _SystemInfoCardState extends State<SystemInfoCard> {
     super.dispose();
   }
 
+  LineChartBarData _ramLine(List<FlSpot> points) {
+    return LineChartBarData(
+      spots: points,
+      dotData: const FlDotData(show: false,),
+      gradient: LinearGradient(
+        colors: [Color(0xFF2196F3).withOpacity(0), Color(0xFF2196F3)],
+        stops: const [0.1, 1.0],
+      ),
+      barWidth: 4,
+      isCurved: false,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('${gpuInfo["VramUsage"]}');
-    // TODO build chart UI
+    return SizedBox(
+      width: 512,
+      height: 256,
+      child: LineChart(
+        LineChartData(
+          minY: 0,
+          maxY: 1,
+          minX: ramPoints.first.x,
+          maxX: ramPoints.last.x,
+          lineTouchData: const LineTouchData(enabled: false),
+          clipData: const FlClipData.all(),
+          gridData: const FlGridData(
+            show: true,
+            drawVerticalLine: false,
+          ),
+          borderData: FlBorderData(show: false),
+          lineBarsData: [
+            _ramLine(ramPoints),
+          ],
+          titlesData: const FlTitlesData(
+            show: false,
+          ),
+        )
+      )
+    );
   }
 }
